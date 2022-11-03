59dd523f0310db2b948855278b5a82ab41555009
albertohcarlos <90426610+albertohcarlos@users.noreply.github.com>
2022-11-03 02:17:58 -0300

    Update productsServices.js

--- src/services/productsServices.js
+++ src/services/productsServices.js
@@ -1,12 +1,7 @@
 import { proxyAPI } from 'config/api';
 
-export const searchQuery = query =>{
-    console.log("REACT_APP_API_URL: " + process.env.REACT_APP_API_URL)
-    proxyAPI.get(`/search/${query}`).then(res => res)
-    .catch(error =>console.log('Error getting search request' + error));
-    
-}
+export const searchQuery = query =>
+    proxyAPI.get(`/search/${query}`).then(res => res);
 
 export const getItem = id =>
-    proxyAPI.get(`/item/${id}`).then(res => res)
-    .catch(()=>console.log('Error getting search request'));
+    proxyAPI.get(`/item/${id}`).then(res => res);
